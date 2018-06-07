50d812a3547d0772cf503b0b9f9a861b8ddea9db
smover <sergio.mover@gmail.com>
2018-06-06 20:41:33 -0600

    Fix start script and download link

--- docker_search/download_data.bash
+++ docker_search/download_data.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export SEARCH_DATA_URL=https://drive.google.com/file/d/1ivVhP5aIeUYBZAV_hdTTIdXirz3XsvUi
+export SEARCH_DATA_URL=https://drive.google.com/file/d/1I43XNDa7ma5niPUsWVh1qEHhGy781sVH
 export SEARCH_ARCHIVE_NAME=sitevisit_extraction.tar.gz2
 
 curl -L https://github.com/circulosmeos/gdown.pl/raw/master/gdown.pl -o gdown.pl \
--- docker_search/wrapper_start.bash
+++ docker_search/wrapper_start.bash
@@ -2,27 +2,11 @@
 
 sudo service ssh start
 
-echo "starting services"
-python /home/biggroum/biggroum/fixr_groum_search_frontend/app.py --host 0.0.0.0 & nohup /home/biggroum/biggroum/FixrService-Backend/target/universal/stage/bin/fixrservice-backend &
+# echo "starting services"
+python /home/biggroum/biggroum/fixr_groum_search_frontend/app.py --host 0.0.0.0 &
+(while true; do sleep 10000; done) | /home/biggroum/biggroum/FixrService-Backend/target/universal/stage/bin/fixrservice-backend &
+
 
 while sleep 60; do
-  ps aux | fixrservice  | grep -q -v grep
-  PROCESS_1_STATUS=$?
-
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 ]; then
-    echo "Fixrservice  has already exited."
-#    exit 1
-  fi
-
-  ps aux | app.py  | grep -q -v grep
-  PROCESS_1_STATUS=$?
-
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 ]; then
-    echo "The web server  has already exited."
-#    exit 1
-  fi
+    echo "Monitoring..."
 done
