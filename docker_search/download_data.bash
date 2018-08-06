06b4b76e4e7de2df1d7033bb810b1f8671bb7503
smover <sergio.mover@gmail.com>
2018-08-05 19:31:45 -0600

    Minor fixes to the config

--- docker_search/download_data.bash
+++ docker_search/download_data.bash
@@ -7,4 +7,8 @@ curl -L https://github.com/circulosmeos/gdown.pl/raw/master/gdown.pl -o gdown.pl
   && perl ./gdown.pl $SEARCH_DATA_URL $SEARCH_ARCHIVE_NAME \
   && tar xjf $SEARCH_ARCHIVE_NAME \
   && rm -rf $SEARCH_ARCHIVE_NAME \
+  && mv sitevisit_extraction ../ \
   && rm gdown.pl
+
+# Note: we move the sitevisit_extraction folder OUTSIDE the docker
+# context, otherwise docker copies all the data in the image!
--- docker_solr/download_data.bash
+++ docker_solr/download_data.bash
@@ -3,8 +3,10 @@
 export SEARCH_DATA_URL=https://drive.google.com/file/d/1LG2e_YMf6ms3ri7s-N34MhaIyfsFYwWD/view?usp=sharing
 export SEARCH_ARCHIVE_NAME=solr_groum.tar.bz2
 
+# Will create a folder solr_groum in ../
 curl -L https://github.com/circulosmeos/gdown.pl/raw/master/gdown.pl -o gdown.pl \
   && perl ./gdown.pl $SEARCH_DATA_URL $SEARCH_ARCHIVE_NAME \
   && tar xjf $SEARCH_ARCHIVE_NAME \
+  && mv solr_groum ../ \
   && rm -rf $SEARCH_ARCHIVE_NAME \
   && rm gdown.pl
