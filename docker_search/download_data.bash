6fdc10a1ce98c9cb4e5e94901dc7fc5554f08851
smover <sergio.mover@gmail.com>
2018-08-06 00:41:06 -0600

    avoid pl script to download files from gdrive

--- docker_search/download_data.bash
+++ docker_search/download_data.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export SEARCH_DATA_URL=https://drive.google.com/file/d/1I43XNDa7ma5niPUsWVh1qEHhGy781sVH
-export SEARCH_ARCHIVE_NAME=sitevisit_extraction.tar.gz2
+# Note: we move the sitevisit_extraction folder OUTSIDE the docker
+# context, otherwise docker copies all the data in the image!
 
-curl -L https://github.com/circulosmeos/gdown.pl/raw/master/gdown.pl -o gdown.pl \
-  && perl ./gdown.pl $SEARCH_DATA_URL $SEARCH_ARCHIVE_NAME \
-  && tar xjf $SEARCH_ARCHIVE_NAME \
+fileid="1I43XNDa7ma5niPUsWVh1qEHhGy781sVH"
+SEARCH_ARCHIVE_NAME="sitevisit_extraction.tar.gz2"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${SEARCH_ARCHIVE_NAME}
+
+tar xjf $SEARCH_ARCHIVE_NAME \
   && rm -rf $SEARCH_ARCHIVE_NAME \
   && mv sitevisit_extraction ../ \
   && rm gdown.pl
-
-# Note: we move the sitevisit_extraction folder OUTSIDE the docker
-# context, otherwise docker copies all the data in the image!
