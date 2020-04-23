26dc1e14563de4b52062c52730a6a81d37de378b
smover <sergio.mover@gmail.com>
2020-04-23 11:33:10 +0200

    Execute the debug server

--- docker_search/wrapper_start.bash
+++ docker_search/wrapper_start.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ./fixrsearch/search_service.py -c /persist/clusters -i /home/biggroum/biggroum/FixrGraphIso/build/src/fixrgraphiso/searchlattice -g /persist/graphs --address 0.0.0.0 -p 8081 -z srcfinder -l 8080
+python ./fixrsearch/search_service.py -c /persist/clusters -i /home/biggroum/biggroum/FixrGraphIso/build/src/fixrgraphiso/searchlattice -g /persist/graphs --address 0.0.0.0 -p 8081 -z srcfinder -l 8080 --debug
 
 while sleep 60; do
     echo "Monitoring..."
